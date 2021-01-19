c430e7c05453ee96caaf7c4ac375510d93f91c49
itbatya <jonbutaev2000@gmail.com>
2021-01-20 05:03:15 +0600

    added main html file

--- book/urls.py
+++ book/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main.views import book_page
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', book_page),
 ]
