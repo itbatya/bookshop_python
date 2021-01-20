6d54c3fb017758cdf6e6167d42ffe7c34f7e161e
itbatya <jonbutaev2000@gmail.com>
2021-01-20 06:12:01 +0600

    created and added model 'Book' to models.py, admin.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    genre = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    year = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
